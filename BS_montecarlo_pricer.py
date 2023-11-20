26a4257a62ee58a738f54619934a47d257adec66
nathank00 <kessellnathan@gmail.com>
2023-11-20 15:58:01 -0700

    python script now functional

--- BS_montecarlo_pricer.py
+++ BS_montecarlo_pricer.py
@@ -8,9 +8,9 @@ import time
 
 class OptionPricing:
 
-    def __init__(self, S0, E, T, rf, sigma, iterations):
+    def __init__(self, S0, K, T, rf, sigma, iterations):
         self.S0 = S0
-        self.E = E
+        self.K = K
         self.T = T
         self.rf = rf 
         self.sigma = sigma 
@@ -28,11 +28,11 @@ class OptionPricing:
         #equation for the S(t) stock price
         stock_price = self.S0 * np.exp(self.T * (self.rf - 0.5 * self.sigma**2) + self.sigma *np.sqrt(self.T) * rand)
 
-        #we need S-E in order to calculate the max(S-E, 0)
-        option_data[:,1] = stock_price - self.E 
+        #we need S-E in order to calculate the max(S-K, 0)
+        option_data[:,1] = stock_price - self.K 
 
         #average for the Monte-Carlo method
-        #np.amax() returns the max(S-E, 0) according to the formula
+        #np.amax() returns the max(S-K, 0) according to the formula
         average = np.sum(np.amax(option_data, axis=1))/float(self.iterations)
 
         #we have to use the exp(-rT) discount factor
@@ -50,11 +50,11 @@ class OptionPricing:
         #equation for the S(t) stock price
         stock_price = self.S0 * np.exp(self.T * (self.rf - 0.5 * self.sigma**2) + self.sigma *np.sqrt(self.T) * rand)
 
-        #we need S-E in order to calculate the max(E-S, 0)
-        option_data[:,1] = self.E - stock_price
+        #we need S-E in order to calculate the max(K-S, 0)
+        option_data[:,1] = self.K - stock_price
 
         #average for the Monte-Carlo method
-        #np.amax() returns the max(E-S, 0) according to the formula
+        #np.amax() returns the max(K-S, 0) according to the formula
         average = np.sum(np.amax(option_data, axis=1))/float(self.iterations)
 
         #we have to use the exp(-rT) discount factor
@@ -64,13 +64,16 @@ class OptionPricing:
 
 if __name__ == "__main__":
 
-    S0 = 100                #underlying stock price at t=0
-    E = 100                 #strike price
-    T = 1                   #expiry
-    rf = 0.05               #risk-free rate
-    sigma = 0.2             #volatility of underlying stock
-    iterations = 10000000   #number of iterations in the monte-carlo simulation
+    ticker = input("Enter Ticker:")
+    print(ticker)
 
-    model = OptionsPricing(S0, E, T, rf, sigma, iterations)
+    S0 = 191.45                #underlying stock price at t=0
+    K = 170                 #strike price
+    T = 0.16438                   #expiry
+    rf = 0.0525               #risk-free rate
+    sigma = 0.2119             #volatility of underlying stock
+    iterations = 1000000   #number of iterations in the monte-carlo simulation
+
+    model = OptionPricing(S0, K, T, rf, sigma, iterations)
     print("Monte-Carlo Call option price: ", model.call_option_simulation())
     print("Monte-Carlo Put option price: ", model.put_option_simulation())
